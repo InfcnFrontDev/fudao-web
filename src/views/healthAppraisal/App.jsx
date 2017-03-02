e8cc51255a4ace9544a07fd7ef141cf495d1746f
wangp <wangp@infcn.com.cn>
2017-03-02 18:44:45 +0800

    assessmentTest.html

--- src/views/healthAppraisal/App.jsx
+++ src/views/healthAppraisal/App.jsx
@@ -6,9 +6,9 @@ export default class App extends React.Component {
         return (
             <div>
                 <div className="appraisalBox">
-
-                    <div  style={{margin:'10px auto',width:220,height:220,display:'flex',justifyContent:'center'}}>
+                    <div  style={{margin:'10px auto',width:220,height:220,display:'flex',justifyContent:'center',position:'relative'}}>
                         <p className="pt-ab fdzs">福道指数</p>
+                        <img src="images/tou.png" style={{marginTop:80,height:80}}/>
                         <svg width="220" height="220" className="pt-ab">
                             <circle  cx="110" cy="110" r="70" strokeWidth="5" stroke="#C9CACA" fill="none"></circle>
                             <circle  cx="110" cy="110" r="70" strokeWidth="5" stroke="#00cccc" fill="none" id="svg_zhengzhuang" transform="matrix(0,-1,1,0,0,220)" ></circle>
@@ -28,6 +28,9 @@ export default class App extends React.Component {
                     </div>
 
                 </div>
+                <button className="appraisalBtn">
+                    一键测评
+                </button>
             </div>
         )
     }
