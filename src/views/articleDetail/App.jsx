c2d06988276be5a7fb9a6c0bb85a3a90c5a8d410
songmiao <songmiao>
2017-03-03 17:23:48 +0800

    arc

--- src/views/articleDetail/App.jsx
+++ src/views/articleDetail/App.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import {
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View
+} from "react-native";
 import styles from "./index.scss";
 
 export default class App extends React.Component {
