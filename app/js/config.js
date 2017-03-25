628a91d1d6b81cee35543d75f9330ab17a7aa12b
杨可可 <yangkk@infcn.com.cn>
2017-03-25 15:44:14 +0800

    修复我的位置不准的问题

--- app/js/config.js
+++ app/js/config.js
@@ -1,21 +1,22 @@
-var apiPath="http://192.168.3.126:18080/fudao-svc/";
-var urls={
-    //生命周期
-    LIFE_CYCLE: apiPath + 'app/myCycleAction!calculateCycleAndSave.action',
-    //天气
-    WEATHER:apiPath +'/app/weatherAction!getWeather.action',
-    //文章详情
-    ARTICLA_DETAIL:apiPath + 'app/myTerritoryAction!detail.action',
-    //添加收藏
-    ADD_COLLECT:apiPath + 'app/collectionAction!addMyCollection.action',
-    //问卷测评
-    ASSESSMENT:apiPath + 'app/questionnaireAction!getQuestionByType.action',
-    //计算得分
-    CALCULATE_SCORE:apiPath +'app/questionnaireAction!calculateScore.action'
+var apiPath = "http://192.168.10.69:8080/fudao-svc/";
+var urls = {
+	//生命周期
+	LIFE_CYCLE: apiPath + 'app/myCycleAction!calculateCycleAndSave.action',
+	//天气
+	WEATHER: apiPath + '/app/weatherAction!getWeather.action',
+	//文章详情
+	ARTICLA_DETAIL: apiPath + 'app/myTerritoryAction!detail.action',
+	//添加收藏
+	ADD_COLLECT: apiPath + 'app/collectionAction!addMyCollection.action',
+	//问卷测评
+	ASSESSMENT: apiPath + 'app/questionnaireAction!getQuestionByType.action',
+	//计算得分
+	CALCULATE_SCORE: apiPath + 'app/questionnaireAction!calculateScore.action'
 }
 
 function getQueryString(name) {
-    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-    var r = window.location.search.substr(1).match(reg);
-    if (r != null) return unescape(r[2]); return null;
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+	var r = window.location.search.substr(1).match(reg);
+	if (r != null) return unescape(r[2]);
+	return null;
 }
