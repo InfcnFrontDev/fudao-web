43d2d3a86f9d160e8e59d1736ce2cb2c5896bb32
杨可可 <yangkk@infcn.com.cn>
2017-04-06 18:39:59 +0800

    config

--- app/js/config.js
+++ app/js/config.js
@@ -1,20 +1,21 @@
-//var apiPath="http://192.168.3.126:18080/fudao-svc/";
-var apiPath = 'http://192.168.10.69:8080/fudao-svc/';
-var urls={
-    //生命周期
-    LIFE_CYCLE: apiPath + 'app/myCycleAction!calculateCycleAndSave.action',
-    //天气
-    WEATHER:apiPath +'/app/weatherAction!getWeather.action',
-    //文章详情
-    ARTICLA_DETAIL:apiPath + 'app/myTerritoryAction!detail.action',
-    //添加收藏
-    ADD_COLLECT:apiPath + 'app/collectionAction!addMyCollection.action',
-    //问卷测评
-    ASSESSMENT:apiPath + 'app/questionnaireAction!getQuestionByType.action',
-    //计算得分
-    CALCULATE_SCORE:apiPath +'app/questionnaireAction!calculateScore.action',
-    //个人习惯（时间段）
-    MYHABIT:apiPath +'app/accountInfoAction!getMyHabit.action'
+// var apiPath = 'http://103.254.113.11:8080/fudao-svc/app/';
+// var apiPath = 'http://192.168.10.69:8080/fudao-svc/app/';
+var apiPath="http://192.168.3.126:18080/fudao-svc/app/";
+var urls = {
+	//生命周期
+	LIFE_CYCLE: apiPath + 'myCycleAction!calculateCycleAndSave.action',
+	//天气
+	WEATHER: apiPath + 'weatherAction!getWeather.action',
+	//文章详情
+	ARTICLA_DETAIL: apiPath + 'myTerritoryAction!detail.action',
+	//添加收藏
+	ADD_COLLECT: apiPath + 'collectionAction!addMyCollection.action',
+	//问卷测评
+	ASSESSMENT: apiPath + 'questionnaireAction!getQuestionByType.action',
+	//计算得分
+	CALCULATE_SCORE: apiPath + 'questionnaireAction!calculateScore.action',
+	//个人习惯（时间段）
+	MYHABIT: apiPath + 'accountInfoAction!getMyHabit.action'
 }
 
 function getQueryString(name) {
