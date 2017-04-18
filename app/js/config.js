c18d0953a49d7fcf76265437bc94d7990d34fcd1
wangp <wangp@infcn.com.cn>
2017-04-18 09:58:25 +0800

    jiehe

--- app/js/config.js
+++ app/js/config.js
@@ -1,23 +1,29 @@
 
 //var apiPath="http://192.168.3.126:18080/fudao-svc/";
 
-var apiPath = 'http://192.168.10.69:8080/fudao-svc/';
+var apiPath = 'http://192.168.10.69:9191/api/';
 //var apiPath = 'http://103.254.113.11:8080/fudao/';
 var urls={
     //生命周期
     LIFE_CYCLE: apiPath + 'app/myCycleAction!calculateCycleAndSave.action',
     //天气
-    WEATHER:apiPath +'/app/weatherAction!getWeather.action',
+    WEATHER:apiPath +'app/weatherAction!getWeather.action',
     //文章详情
-    ARTICLA_DETAIL:apiPath + 'app/myTerritoryAction!detail.action',
+    ARTICLE_GETARTICLE:apiPath + 'app/ArticleApi/getArticle',
     //添加收藏
-    ADD_COLLECT:apiPath + 'app/collectionAction!addMyCollection.action',
+    COLLECTION_ADDMYCOLLECTION:apiPath + 'CollectionApi/addMyCollection',
     //问卷测评
     ASSESSMENT:apiPath + 'app/questionnaireAction!getQuestionByType.action',
     //计算得分
     CALCULATE_SCORE:apiPath +'app/questionnaireAction!calculateScore.action',
-    //个人习惯（时间段）
-    MYHABIT:apiPath +'app/accountInfoAction!getMyHabit.action'
+    //我的时间分段
+    TIMEPERIOD_GETMYTIMEPERIOD:apiPath+'TimePeriodApi/getMyTimePeriod',
+    //我的时间阶段
+    TIMEPERIOD_GETMYTIMESTAGE:apiPath+'TimePeriodApi/getMyTimeStage',
+    //通用时间分段
+    TIMEPERIOD_GETGENERALTIMEPERIOD:apiPath +'TimePeriodApi/getGeneralTimePeriod',
+    //通用时间阶段
+    TIMEPERIODAPI_GETGENERALTIMESTAGE:apiPath+'TimePeriodApi/getGeneralTimeStage',
 }
 
 function getQueryString(name) {
