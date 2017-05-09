4436d5784bc2f56a878828fc6d7385540cbd473c
chenxx <18301203128@163.com>
2017-05-09 10:40:43 +0800

    .

--- app/js/config.js
+++ app/js/config.js
@@ -1,34 +1,42 @@
-
 //var apiPath="http://192.168.3.126:18080/fudao-svc/";
 
 var apiPath = 'http://192.168.10.69:9191/api/';
 //var apiPath = 'http://103.254.113.11:8080/fudao/';
-var urls={
+var urls = {
     //生命周期
     LIFE_CYCLE: apiPath + 'app/myCycleAction!calculateCycleAndSave.action',
     //天气
-    WEATHER:apiPath +'WeatherApi/getWeather',
+    WEATHER: apiPath + 'WeatherApi/getWeather',
     //文章详情
-    ARTICLE_GETARTICLE:apiPath + 'ArticleApi/getArticle',
+    ARTICLE_GETARTICLE: apiPath + 'ArticleApi/getArticle',
     //添加收藏
-    COLLECTION_ADDMYCOLLECTION:apiPath + 'CollectionApi/addMyCollection',
+    COLLECTION_ADDMYCOLLECTION: apiPath + 'CollectionApi/addMyCollection',
     //问卷测评
-    ASSESSMENT:apiPath + 'app/questionnaireAction!getQuestionByType.action',
+    ASSESSMENT: apiPath + 'app/questionnaireAction!getQuestionByType.action',
     //计算得分
-    CALCULATE_SCORE:apiPath +'app/questionnaireAction!calculateScore.action',
+    CALCULATE_SCORE: apiPath + 'app/questionnaireAction!calculateScore.action',
     //我的时间分段
-    TIMEPERIOD_GETMYTIMEPERIOD:apiPath+'TimePeriodApi/getMyTimePeriod',
+    TIMEPERIOD_GETMYTIMEPERIOD: apiPath + 'TimePeriodApi/getMyTimePeriod',
     //我的时间阶段
-    TIMEPERIOD_GETMYTIMESTAGE:apiPath+'TimePeriodApi/getMyTimeStage',
+    TIMEPERIOD_GETMYTIMESTAGE: apiPath + 'TimePeriodApi/getMyTimeStage',
     //通用时间分段
-    TIMEPERIOD_GETGENERALTIMEPERIOD:apiPath +'TimePeriodApi/getGeneralTimePeriod',
+    TIMEPERIOD_GETGENERALTIMEPERIOD: apiPath + 'TimePeriodApi/getGeneralTimePeriod',
     //通用时间阶段
-    TIMEPERIODAPI_GETGENERALTIMESTAGE:apiPath+'TimePeriodApi/getGeneralTimeStage',
+    TIMEPERIODAPI_GETGENERALTIMESTAGE: apiPath + 'TimePeriodApi/getGeneralTimeStage',
+    //获取问卷列表
+    DIAGNOSIS_GETQUESTIONNAIRELIST: apiPath + 'DiagnosisApi/getQuestionnaireList',
+    //提交问卷
+    DIAGNOSIS_SUBMITQUESTIONNAIRERESULT: apiPath + 'DiagnosisApi/submitQuestionnaireResult',
+    //获取测评结果
+    DIAGNOSIS_GETEVALUATIONRESULT: apiPath + 'DiagnosisApi/getEvaluationResult',
+    //获取图片
+    IMGSRC:apiPath+'ImgApi/getImage?filePath=',
+
 }
 
 function getQueryString(name) {
-	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-	var r = window.location.search.substr(1).match(reg);
-	if (r != null) return unescape(r[2]);
-	return null;
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]);
+    return null;
 }
