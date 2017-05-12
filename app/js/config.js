409a8be8bb11b26cade988330bb9d33ef6657760
wangp <wangp@infcn.com.cn>
2017-05-12 15:21:30 +0800

    config

--- app/js/config.js
+++ app/js/config.js
@@ -1,7 +1,7 @@
 //var apiPath="http://192.168.3.126:18080/fudao-svc/";
 
-var apiPath = 'http://192.168.10.69:9191/api/';
-//var apiPath = 'http://103.254.113.11:8080/fudao/';
+//var apiPath = 'http://192.168.10.69:9191/api/';
+var apiPath = 'http://103.254.113.11:9191/api/';
 var urls = {
     //生命周期
     LIFE_CYCLE: apiPath + 'app/myCycleAction!calculateCycleAndSave.action',
